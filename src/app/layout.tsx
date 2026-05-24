26d4aab2e643d4e6b200677f62386655433bdc25
ws0x <yusufnaeemhakim@gmail.com>
2026-05-24 08:48:17 +0300

    feat: Integrating GA4 & GSC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -57,6 +58,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
+      <GoogleAnalytics />
       <body className="antialiased">{children}</body>
     </html>
   );
