22914df67903bdadc008d8cba4de200f16d20920
ws0x <yusufnaeemhakim@gmail.com>
2026-06-02 15:05:31 +0300

    fix: fixing favicon.ico

--- src/app/favicon.ico/route.ts
+++ src/app/favicon.ico/route.ts
@@ -2,7 +2,7 @@ import { readFile } from "fs/promises";
 import { join } from "path";
 
 export async function GET() {
-  const filePath = join(process.cwd(), "public", "favicon.png");
+  const filePath = join(process.cwd(), "public", "favicon.ico");
   const buffer = await readFile(filePath);
   return new Response(buffer, {
     headers: {
